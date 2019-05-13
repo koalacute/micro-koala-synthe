9bc2edba5f9a3533411ccd13f7c593e306479f8d
guoyuqiang <guoyuqiang@stormbright.com>
2019-05-13 19:23:32 +0800

    init synthe server

---
+++ src/main/java/com/koalacute/micro/koala/synthe/MicroKoalaSyntheApplication.java
@@ -0,0 +1,13 @@
+package com.koalacute.micro.koala.synthe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroKoalaSyntheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroKoalaSyntheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koalacute/micro/koala/synthe/MicroKoalaSyntheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.koalacute.micro.koala.synthe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroKoalaSyntheApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
