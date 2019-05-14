94b6129e9db068963d5302439147c911219001d1
guoyuqiang <guoyuqiang@stormbright.com>
2019-05-14 09:20:56 +0800

    基础服务配置

--- src/main/java/com/koalacute/micro/koala/synthe/MicroKoalaSyntheApplication.java
+++ src/main/java/com/koalacute/micro/koala/synthe/MicroKoalaSyntheApplication.java
@@ -1,9 +1,17 @@
 package com.koalacute.micro.koala.synthe;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
+@EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.koalacute.micro.koala.synthe.mapper")
+@ComponentScan({"com.koalacute.micro"})
+@PropertySource(value = "classpath:/config/*.yml", ignoreResourceNotFound = true)
 public class MicroKoalaSyntheApplication {
 
     public static void main(String[] args) {
